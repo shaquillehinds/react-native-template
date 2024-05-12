d75b8d8a13a079371d9258dd7f83f1acfcfe807f
Shaquille <shaqdulove@hotmail.com>
2024-05-12 14:26:58 -0400

    Android reset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  overrides: [
-    {
-      files: ['*.js', '*.ts', '*.tsx', '*.mts'],
-      rules: {
-        'no-undef': 0,
-        'no-unused-vars': 0,
-        '@typescript-eslint/no-unused-vars': 1,
-        'prettier/prettier': 0,
-        'no-trailing-spaces': 0,
-        'react-hooks/exhaustive-deps': 0,
-        curly: 0,
-      },
-    },
-  ],
 };
