a6879e61c8349abbeaf8730868ff30e3081fc638
Shaquille <shaqdulove@hotmail.com>
2024-05-13 18:43:18 -0400

    Bug fixes in newProject.sh

--- scripts/newProject/newProject.sh
+++ scripts/newProject/newProject.sh
@@ -29,6 +29,8 @@ cp -r reactNativeTemplate/android/app/src/main/java/com/reactnativetemplate/Main
 cp -r reactNativeTemplate/ios/Podfile $name/ios
 cp -r reactNativeTemplate/ios/reactNativeTemplate/{Images.xcassets,AppDelegate.h,AppDelegate.mm,LaunchScreen.storyboard,SplashScreen.storyboard} $name/ios/$name
 
+# Renaming reactNativeTemplate
+
 # package.json [common]
 sed "s/reactnativetemplate/$lowerName/ig" reactNativeTemplate/package.json > $name/package.json
 # android/settings.gradle
@@ -39,11 +41,17 @@ sed "s/reactnativetemplate/$lowerName/ig" reactNativeTemplate/android/app/build.
 sed "s/reactnativetemplate/$lowerName/ig" reactNativeTemplate/android/app/src/main/AndroidManifest.xml > $name/android/app/src/main/AndroidManifest.xml
 # android/app/src/main/java/com/nameofproject/MainApplication.kt [common]
 sed "s/reactnativetemplate/$lowerName/ig" reactNativeTemplate/android/app/src/main/java/com/reactnativetemplate/MainApplication.kt > $name/android/app/src/main/java/com/$lowerName/MainApplication.kt
-# android/app/src/main/java/com/nameofproject/MainActivity.kt [Both]
+android/app/src/main/java/com/nameofproject/MainActivity.kt [Both]
 sed "s/reactnativetemplate/$lowerName/g" reactNativeTemplate/android/app/src/main/java/com/reactnativetemplate/MainActivity.kt > $name/android/app/src/main/java/com/$lowerName/MainActivity.kt
-sed "s/reactNativeTemplate/$name/g" reactNativeTemplate/android/app/src/main/java/com/reactnativetemplate/MainActivity.kt > $name/android/app/src/main/java/com/$lowerName/MainActivity.kt
+mv $name/android/app/src/main/java/com/$lowerName/MainActivity.kt $name/android/app/src/main/java/com/$lowerName/MainActivity2.kt
+sed "s/reactNativeTemplate/$name/g" $name/android/app/src/main/java/com/$lowerName/MainActivity2.kt > $name/android/app/src/main/java/com/$lowerName/MainActivity.kt
+rm $name/android/app/src/main/java/com/$lowerName/MainActivity2.kt
+# android/app/src/main/res/values/strings.xml
+sed "s/reactNativeTemplate/$name/g" reactNativeTemplate/android/app/src/main/res/values/strings.xml > $name/android/app/src/main/res/values/strings.xml
 # ios/Podfile
 sed "s/reactNativeTemplate/$name/g" reactNativeTemplate/ios/Podfile > $name/ios/Podfile
+# ios/nameOfProject/LaunchScreen.storyboard
+sed "s/reactNativeTemplate/$name/g" reactNativeTemplate/ios/reactNativeTemplate/LaunchScreen.storyboard > $name/ios/$name/LaunchScreen.storyboard
 # ios/nameOfProject/AppDelegate.mm
 sed "s/reactNativeTemplate/$name/g" reactNativeTemplate/ios/reactNativeTemplate/AppDelegate.mm > $name/ios/$name/AppDelegate.mm
 # ios/nameOfProject/Info.plist
