4ee685de924681c23367f01e388dda8e4caa8784
Shaquille <shaqdulove@hotmail.com>
2024-05-14 14:00:44 -0400

    HttpClient update

--- src/transports/HttpClient.ts
+++ src/transports/HttpClient.ts
@@ -1,7 +1,17 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import axios, {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from 'axios';
 
 interface Interceptors {
   handleResponse?: ({ data }: AxiosResponse) => any;
+  handleRequest?: (
+    value: InternalAxiosRequestConfig<any>,
+  ) =>
+    | InternalAxiosRequestConfig<any>
+    | Promise<InternalAxiosRequestConfig<any>>;
   handleError?: (error: AxiosError) => any;
 }
 
@@ -20,9 +30,14 @@ export class HttpClient {
       interceptors?.handleResponse || this.handleResponse,
       interceptors?.handleError || this.handleError,
     );
+    this.instance.interceptors.request.use(
+      interceptors?.handleRequest || this.handleRequest,
+      interceptors?.handleError || this.handleError,
+    );
   };
 
   private handleResponse = ({ data }: AxiosResponse) => data;
+  private handleRequest = (value: InternalAxiosRequestConfig<any>) => value;
 
   protected handleError = (error: AxiosError) => {
     return Promise.reject(error);
